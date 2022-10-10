c023aa9b41e9d56939419257d97f4ffcb9423f2f
AdilMango <adil.basha@mangoleap.com>
2022-10-10 23:33:52 +0530

    [feat] - Core providers for http & storage

---
+++ lib/core/services/http/http_service_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gallabox/core/services/http/dio_http_service.dart';
+import 'package:gallabox/core/services/http/http_service.dart';
+import 'package:gallabox/core/services/storage/storage_service_provider.dart';
+
+/// Provider that maps an [HttpService] interface to implementation
+final httpServiceProvider = Provider<HttpService>((ref) {
+  final storageService = ref.watch(storageServiceProvider);
+  return DioHttpService(storageService);
+});
